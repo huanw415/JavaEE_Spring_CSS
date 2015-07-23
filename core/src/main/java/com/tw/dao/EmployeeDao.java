d9c2583894883fe0b7e564d3ac12053168b1270f
hawang <654296217@qq.com>
2015-07-23 15:50:40 +0800

    delete useless codes

--- core/src/main/java/com/tw/dao/EmployeeDao.java
+++ core/src/main/java/com/tw/dao/EmployeeDao.java
@@ -43,14 +43,8 @@ public class EmployeeDao {
 
     public void updateEmployee(Employee employee){
         Session session = HibernateUtil.getSessionFactory().getCurrentSession();
-        session.beginTransaction();
-//        Query query = session.createQuery("update Employee t set t.name=:name, t.role=:role where id=:id");
-//
-//        query.setInteger("id", employee.getId());
-//        query.setString("name", employee.getName());
-//        query.setString("role", employee.getRole());
 
-//        query.executeUpdate();
+        session.beginTransaction();
         session.update(employee);
         session.getTransaction().commit();
     }
@@ -76,46 +70,5 @@ public class EmployeeDao {
 
         return employee;
     }
-//    public User getUserById(int id) {
-//
-//        Session session = HibernateUtil.getSessionFactory().getCurrentSession();
-//
-//        session.beginTransaction();
-//
-//        String hql = "from User where id=:id";
-//        Query query = session.createQuery(hql);
-//        query.setInteger("id", id);
-//
-//        User user = (User)query.list().get(0);
-//
-//        session.getTransaction().commit();
-//
-//        return user;
-//    }
-
 }
 
-//public class EmployeeDao extends HibernateDaoSupport {
-//
-//    private SessionFactory sessionFactory;
-//
-//    @Autowired
-//    public EmployeeDao(SessionFactory sessionfactory){
-//        this.sessionFactory = sessionfactory;
-//        setSessionFactory(sessionfactory);
-//    }
-//
-//
-//    public List<Employee> getAllEmployees(){
-//System.out.println("++++++++++++++++++" + sessionFactory +"++++++++++++++++++");
-////        Session session = getSessionFactory().getCurrentSession();
-////        Session session = HibernateUtil.getSessionFactory().getCurrentSession();
-////        sessionFactory.openSession();
-////        session.beginTransaction();
-//        List<Employee> employees = (List<Employee>)getHibernateTemplate().find("from Employee");
-////        List<Employee> employees = session.createQuery("FROM Employee").list();
-////        session.getTransaction().commit();
-//
-//        return employees;
-//    }
-//}
