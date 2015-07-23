d948dffb4019002b2c29463d2f59703ed295b26f
hawang <654296217@qq.com>
2015-07-23 14:30:25 +0800

    modify user and employee structure

---
+++ core/src/main/resources/db/migration/V1__Create_Employee_table.sql
@@ -0,0 +1,20 @@
+USE users_management;
+
+-- CREATE TABLE EMPLOYEE
+-- (
+--   Id INT AUTO_INCREMENT primary key,
+--   Name VARCHAR(50) NOT NULL,
+--   Role VARCHAR(10) NOT NULL,
+--   UserId INT,
+--   FOREIGN KEY (UserId) REFERENCES USER(Id) ON DELETE SET NULL
+-- );
+
+CREATE TABLE EMPLOYEE
+(
+  Id INT AUTO_INCREMENT primary key,
+  Name VARCHAR(50) NOT NULL,
+  Role VARCHAR(10) NOT NULL,
+  Gender VARCHAR(10) NOT NULL,
+  Email VARCHAR(100) NOT NULL,
+  Age INT NOT NULL
+);
