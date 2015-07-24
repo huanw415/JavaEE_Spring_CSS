fd071331939fc276f48cc81fa270e30eed5cf5b3
hawang <654296217@qq.com>
2015-07-24 16:32:33 +0800

    add addNewUser method

--- web/src/main/webapp/angular/js/controllers/usersController.js
+++ web/src/main/webapp/angular/js/controllers/usersController.js
@@ -1,9 +1,18 @@
 'use strict';
 
 angular.module('users_management')
-    .controller('usersController', function($scope, $http){
+    .controller('usersController', function($scope, $http, $location){
 
         $http.get('/web/api/users').success(function(users){
             $scope.users = users;
         });
+
+        $scope.addNewUser = function(){
+
+            $http.get('/web/api/employees').success(function(employees){
+
+                $scope.employees = employees;
+                $location.path('/userCreation');
+            });
+        };
     });
