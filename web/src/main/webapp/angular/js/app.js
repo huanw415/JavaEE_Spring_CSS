3a31a5e0472d0d947b2773355dc28202c3c038ba
hawang <654296217@qq.com>
2015-07-24 14:45:50 +0800

    add navigator

--- web/src/main/webapp/angular/js/app.js
+++ web/src/main/webapp/angular/js/app.js
@@ -3,10 +3,6 @@
 angular.module('users_management', ['ngRoute'])
     .config(function($routeProvider){
         $routeProvider
-            .when('/', {
-                templateUrl: 'views/users.html',
-                controller: 'usersController'
-            })
             .when('/users', {
                 templateUrl: 'views/users.html',
                 controller: 'usersController'
