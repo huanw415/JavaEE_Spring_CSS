8b301deeec7fe87cc32e32b6e0f406071c68aae8
hawang <654296217@qq.com>
2015-07-26 18:00:23 +0800

    finish the request of employee creation

--- core/src/main/java/com/tw/controller/angular_controller/EmployeesController.java
+++ core/src/main/java/com/tw/controller/angular_controller/EmployeesController.java
@@ -3,12 +3,11 @@ package com.tw.controller.angular_controller;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.tw.Util.HibernateProxyTypeAdapter;
+import com.tw.entity.Employee;
 import com.tw.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * Created by hgwang on 7/24/15.
@@ -28,5 +27,10 @@ public class EmployeesController {
     public @ResponseBody
     String getAllEmployees(){
         return gson.toJson(employeeService.getAllEmployees());
+    }
+
+    @RequestMapping(method = RequestMethod.POST, produces= MediaType.APPLICATION_JSON_VALUE)
+    public void createEmployee(@RequestBody Employee employee){
+        employeeService.createEmployee(employee);
     }
 }
--- web/src/main/webapp/angular/js/controllers/employeeCreationController.js
+++ web/src/main/webapp/angular/js/controllers/employeeCreationController.js
@@ -1,6 +1,12 @@
 'user strict';
 
 angular.module('users_management')
-    .controller('employeeCreationController', function ($scope, $http) {
-
+    .controller('employeeCreationController', function ($scope, $http, $location) {
+        $scope.createEmployee = function (employee) {
+            $http.post('/web/api/employees',
+                { name: employee.name, role: employee.role, gender: employee.gender, email: employee.email, age: employee.age})
+                .success(function(){
+                   $location.path('/employees');
+                });
+        }
     });
