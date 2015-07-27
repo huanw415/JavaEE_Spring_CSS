289d8daf9936b31d54c005168c5683c6fcb239ad
hawang <654296217@qq.com>
2015-07-27 18:06:12 +0800

    implment the post request of get customers

--- core/src/main/java/com/tw/controller/angular_controller/CustomersController.java
+++ core/src/main/java/com/tw/controller/angular_controller/CustomersController.java
@@ -1,7 +1,38 @@
 package com.tw.controller.angular_controller;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.tw.Util.HibernateProxyTypeAdapter;
+import com.tw.service.CustomerService;
+import com.tw.service.EmployeeService;
+import flexjson.JSONSerializer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * Created by hgwang on 7/27/15.
  */
+
+@RestController
+@RequestMapping(value = "/api/customers")
 public class CustomersController {
+
+    @Autowired
+    private CustomerService customerService;
+
+    @Autowired
+    private EmployeeService employeeService;
+
+    private Gson gson = new GsonBuilder().setExclusionStrategies()
+            .registerTypeAdapterFactory(HibernateProxyTypeAdapter.FACTORY)
+            .create();
+
+    @RequestMapping(method = RequestMethod.GET)
+    public @ResponseBody
+    String getAllEmployees(){
+        return new JSONSerializer().include("courses").serialize(customerService.getAllCustomers());
+    }
 }
--- web/src/main/webapp/angular/js/controllers/customersController.js
+++ web/src/main/webapp/angular/js/controllers/customersController.js
@@ -4,7 +4,19 @@ angular.module('users_management')
     .controller('customersController', function ($scope, $http) {
 
         $http.get('/web/api/customers').success(function (customers) {
-            console.log(customers);
+
+            for(var i=0; i<customers.length; i++){
+
+                var courses = customers[i].courses;
+
+                for(var j=0; j<courses.length; j++){
+
+                    if(courses[j].name === 'private'){
+                        customers[i].privateCoach = courses[j].employee.name;
+                    }
+                }
+            }
+
             $scope.customerObjects = customers;
         });
     });
