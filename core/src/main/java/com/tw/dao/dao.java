9a39c1e599bfed60389404516b2dedef13f41f26
hawang <654296217@qq.com>
2015-08-03 16:39:45 +0800

    define dao interface

--- core/src/main/java/com/tw/dao/dao.java
+++ core/src/main/java/com/tw/dao/dao.java
@@ -1,7 +1,25 @@
 package com.tw.dao;
 
+import com.tw.entity.User;
+
+import java.util.List;
+
 /**
  * Created by hgwang on 8/3/15.
  */
-public interface dao {
+public interface Dao<T> {
+
+    List<T> getDataList(Class<T> tClass);
+
+    T getDataById(int id, Class<T> tClass);
+
+    T getDataByName(String name, Class<T> tClass);
+
+    void createData(T data);
+
+    void deleteData(T data);
+
+    void updateData(T data, Class<T> tClass);
+
+    void updateUser(User user);
 }
