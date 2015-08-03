987a3eb36e1389c1a9bfd969aee0157050dad893
hawang <654296217@qq.com>
2015-08-03 16:42:20 +0800

    replace userDao with unifyDao

--- core/src/main/java/com/tw/service/UserService.java
+++ core/src/main/java/com/tw/service/UserService.java
@@ -1,6 +1,6 @@
 package com.tw.service;
 
-import com.tw.dao.UserDao;
+import com.tw.dao.Dao;
 import com.tw.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,38 +11,38 @@ import java.util.List;
 public class UserService {
 
     @Autowired
-    private UserDao userDao;
+    private Dao<User> userDao;
 
     public List<User> getAllUsers() {
 
-        return userDao.getAllUsers();
+        return userDao.getDataList(User.class);
     }
 
     public void createUser(User user) {
-        userDao.createUser(user);
+        userDao.createData(user);
     }
 
     public User getUserById(int id) {
-        return userDao.getUserById(id);
+        return userDao.getDataById(id, User.class);
     }
 
     public void deleteUser(User user) {
-        userDao.deleteUser(user);
+        userDao.deleteData(user);
     }
 
     public void updateUser(User user) {
         userDao.updateUser(user);
     }
 
-    public List<User> getUsersByName(String name) {
-        return userDao.getUsersByName(name);
-    }
-
-    public String canLogIn(User user, String password) {
-        if (user.getPassword().equals(password)) {
-            return "密码正确";
-        } else {
-            return "密码错误";
-        }
-    }
+//    public List<User> getUsersByName(String name) {
+//        return userDao.getUsersByName(name);
+//    }
+//
+//    public String canLogIn(User user, String password) {
+//        if (user.getPassword().equals(password)) {
+//            return "密码正确";
+//        } else {
+//            return "密码错误";
+//        }
+//    }
 }
